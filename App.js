fcad3f2b38188244ec4e3c038324d7210b6ded2b
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-05-24 14:32:41 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Button } from "react-native";
+import GoalInput from "./components/GoalInput";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+  const addGoalHandler = (enteredGoal) => {
+    if (enteredGoal.length === 0) {
+      return;
+    }
+    setCourseGoals((courseGoals) => [
+      ...courseGoals,
+      { id: Math.random().toString(), value: enteredGoal },
+    ]);
+    setIsAddMode(false);
+  };
+  const handleDelete = (goalId) => {
+    setCourseGoals((courseGoals) => {
+      return courseGoals.filter((goal) => {
+        return goal.id !== goalId;
+      });
+    });
+  };
+  const cancelAddGoal = () => {
+    setIsAddMode(false);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        addGoalHandler={addGoalHandler}
+        isAddMode={isAddMode}
+        cancelAddGoal={cancelAddGoal}
+      />
+      <GoalItem courseGoals={courseGoals} handleDelete={handleDelete} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
 });
